289e0a52d3f74d6c34a4fcdf5533024f7fd095e5
Sungjun Hong <hsungjun@myseneca.ca>
2018-05-25 21:44:31 -0400

    d

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var sha256 = require('sha256');
 var salt = 'fEWFGEG#3543fdfweq#$R@#';
 var session = require('express-session');
 var mysql = require('mysql');
+var fs = require('fs');
 
 // clearmysql을 내 heroku app에 설정하고, 해당 정보를 하기와 같이 기재. 이후, navigator를 이용해서 테이블 수정
 var conn = mysql.createConnection({
@@ -18,6 +19,19 @@ var conn = mysql.createConnection({
 });
 
 conn.connect();
+
+
+app.use(express.static('./sub')); // In order to use CSS file
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+    secret : 'defWgw#$@$fadd1123',
+    resave : false,
+    saveUninitialized : true
+}));
+
+// app.set('view engine', 'jade');
+// app.set('views', './sub');
+
 
 app.get('/', (req, res) => {
     var sql = 'SELECT *FROM user';
