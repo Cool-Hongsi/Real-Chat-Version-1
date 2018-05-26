09702721161e63e13e5e7393d8674d268492e2ca
Sungjun Hong <hsungjun@myseneca.ca>
2018-05-26 01:27:35 -0400

    c

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ io.on('connection', function(socket){
         updateNicknames();
       }
     })
-
+    // aaaaaa
     function updateNicknames(){
         io.emit('usernames', nicknames);
     }
