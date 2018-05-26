663b5f39d115e206fd716989f939b89036c41e39
Sungjun Hong <hsungjun@myseneca.ca>
2018-05-26 02:23:43 -0400

    c

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var fs = require('fs');
 var ejs = require('ejs');
 // var modulefile = require('./module.js');
 
+/*
 // clearmysql을 내 heroku app에 설정하고, 해당 정보를 하기와 같이 기재. 이후, navigator를 이용해서 테이블 수정
 var conn = mysql.createConnection({
     host : 'localhost', // 'us-cdbr-iron-east-04.cleardb.net', // 'localhost',
@@ -21,6 +22,7 @@ var conn = mysql.createConnection({
 });
 
 conn.connect();
+*/
 
 app.use(express.static('./sub')); // In order to use CSS file
 app.use(bodyParser.urlencoded({extended: false}));
