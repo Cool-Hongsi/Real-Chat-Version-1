e30ee607575921108bdec6a8ac8eeec0dc374a38
Sungjun Hong <hsungjun@myseneca.ca>
2018-05-26 02:03:31 -0400

    c

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ var fs = require('fs');
 var ejs = require('ejs');
 // var modulefile = require('./module.js');
 
-
 // clearmysql을 내 heroku app에 설정하고, 해당 정보를 하기와 같이 기재. 이후, navigator를 이용해서 테이블 수정
 var conn = mysql.createConnection({
-    host : 'us-cdbr-iron-east-04.cleardb.net', // 'localhost',
-    user : 'b0dd242e3864eb', // 'root', 
-    password : '811489ba', // '111111', 
-    database : 'heroku_e32bdbd71a00947' // 'signup'
+    host : 'localhost', // 'us-cdbr-iron-east-04.cleardb.net', // 'localhost',
+    user : 'root', // 'b0dd242e3864eb', // 'root', 
+    password : '111111', // '811489ba', // '111111', 
+    database : 'signup' // 'heroku_e32bdbd71a00947' // 'signup'
 });
 
 conn.connect();
