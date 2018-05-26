5223fabaa0a7e3af7d1503bc3f259c1cd30b6dec
Sungjun Hong <hsungjun@myseneca.ca>
2018-05-25 21:10:07 -0400

    c

--- server.js
+++ server.js
@@ -9,19 +9,27 @@ var salt = 'fEWFGEG#3543fdfweq#$R@#';
 var session = require('express-session');
 var mysql = require('mysql');
 
-
+// clearmysql을 내 heroku app에 설정하고, 해당 정보를 하기와 같이 기재. 이후, navigator를 이용해서 테이블 수정
 var conn = mysql.createConnection({
-    host : 'us-cdbr-iron-east-04.cleardb.net', // 'localhost',
-    user : 'b0dd242e3864eb', // 'root',
-    password : '811489ba', // '111111',
-    database : 'heroku_e32bdbd71a00947' // 'signup'
+    host : 'us-cdbr-iron-east-04.cleardb.net',
+    user : 'b0dd242e3864eb',
+    password : '811489ba',
+    database : 'heroku_e32bdbd71a00947'
 });
 
 conn.connect();
 
 
 app.get('/', (req, res) => {
-    res.send('HI !!');
+    var sql = 'SELECT *FROM user';
+    conn.query(sql, function(err, rows, fields){
+        if(err){
+            throw err;
+        }
+        else{
+            res.send(rows[0].NAME);
+        }
+    })
 })
 
 app.listen(port, () => {
